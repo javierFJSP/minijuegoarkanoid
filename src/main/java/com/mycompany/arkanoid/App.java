797e9acb37369021f5a75ef14598609690ad1e3b
Javier <Javier@LAPTOP-MBP5N9U7>
2020-01-27 10:30:39 +0100

    arkanoid

--- src/main/java/com/mycompany/arkanoid/App.java
+++ src/main/java/com/mycompany/arkanoid/App.java
@@ -4,9 +4,12 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -14,19 +17,27 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    int Scene_width = 240;
-    int Scene_height = 320;
+    int Scene_width = 340;
+    int Scene_height = 420;
+    short stickHeight = 50;
     
     @Override
     public void start(Stage stage) {
         
         Pane root = new Pane();
         var scene = new Scene(root, Scene_width, Scene_height);
-        scene.setFill(Color.BLACK);
         stage.setScene(scene);
         stage.show();
         
+        //fondo del juego
+        Image fondo = new Image(getClass().getResourceAsStream("/imagenes/fondo.png"));
+        ImageView imageView1 = new ImageView(fondo);
+        root.getChildren().add(imageView1);
         
+        //barra de control
+        Image barra = new Image(getClass().getResourceAsStream("/imagenes/barracontrol.PNG"));
+        ImageView imageView2 = new ImageView(barra);
+        root.getChildren().add(imageView2);
     
         
         
@@ -45,7 +56,11 @@ public class App extends Application {
         
         
         
+        Timeline timeline = new Timeline(
         
+        
+        
+        );
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
     }
@@ -55,3 +70,7 @@ public class App extends Application {
     }
 
 }
+
+
+
+
